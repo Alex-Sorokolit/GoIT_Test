609ed4270f9a22c51fcf9b79ab71dc9330fd9363
Alex-Sorokolit <sorokolital@gmail.com>
2023-04-29 12:21:02 +0300

    map data

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export async function getUsers(controller) {
   try {
     const url = "https://63d3d5b3a93a149755b3fa18.mockapi.io/users";
-    // console.log("controller: ", controller);
     const response = axios.get(url, { signal: controller.signal });
 
     return response;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,36 +1,65 @@
 import React, { useEffect, useState } from "react";
-// import Card from "./Card";
 import { getUsers } from "api/api";
+import Card from "./Card";
 
 const List = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Відхилення запиту
     const controller = new AbortController();
 
-    // Отримуємо дані з бекенду
     async function fetchData() {
+      setIsLoading(true);
+      setError(null);
+
+      // Запит на бекенд
       try {
         const usersData = await getUsers(controller);
-        console.log("usersData:", JSON.stringify(usersData, null, 2));
-        setUsers(usersData);
+        // console.log("usersData:", JSON.stringify(usersData, null, 2));
+
+        // Перевірка чи є результати пошуку
+        if (usersData.length === 0) {
+          console.log("немає даних");
+          setError("Щось пішло не так, спробуйте перезавантажити сторінку");
+          return;
+        }
+
+        // Записуємо дані у стейт
+        setUsers(usersData.data);
       } catch (error) {
-        console.log(error.message);
+        console.log("catch: ", error.message);
+        setError("Щось пішло не так, перезавантажте сторінку");
+      } finally {
+        setIsLoading(false);
       }
     }
+
     fetchData();
 
     // Відхиляє запит при розмонтуванні елементу
     return () => {
       controller.abort();
     };
-  }, [setUsers]);
+  }, []);
+
+  if (!users) {
+    return null;
+  }
 
   return (
     <>
-      <h1>Hello!</h1>
-      {users.length > 0 && <p>You have {users.length} unread messages.</p>}
+      <h1>List</h1>
+      {users.length > 0 &&
+        users.map((user) => (
+          <li key={user.id}>
+            <Card />
+          </li>
+        ))}
+      {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
     </>
   );
 };
